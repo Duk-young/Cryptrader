03493ac0d3578a4007aab4c5b40220973eaa6d12
DukyoungEom <79042508+DukyoungEom@users.noreply.github.com>
2021-05-20 19:43:30 +0900

    WebSocket Libraries, Upbit Real time price snapshot test

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from flask import g
 import jwt   # PyJWT
 import uuid
 import requests
-from flask_wtf import Form
+import websocket
+import json
+from flask_socketio import SocketIO, send, emit
+from eventlet import wsgi
+import eventlet
 
+DATABASE = 'deomps5.db'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+socketio = SocketIO(app)
 #UPBIT ACCESS
 #Access Key : dqZ29pX3nfA8KyxmiYyRt9UwzBOW4a8TwRs34Er7
 #Secret Key : V6Hd527RnXmqsEbq1qP6sMBpxw0po7zjbBwHr9L4
@@ -16,14 +26,37 @@ payload = {
 }
 jwt_token = jwt.encode(payload, 'V6Hd527RnXmqsEbq1qP6sMBpxw0po7zjbBwHr9L4')
 authorization_token = 'Bearer {}'.format(jwt_token)
+
+## Upbit Realtime Price check
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+def on_message(ws, message):
+    get_message = json.loads(message)
+    print(get_message["code"],'\'s Real Time Price: ', get_message["trade_price"])
+    handle_json(get_message);
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("close")
+
+def on_open(ws):
+    def run():
+      while(True):
+        sendData = '[{"ticket":"test"},{"type":"ticker","codes":["KRW-BTC","KRW-ADA"],"isOnlySnapshot":true}]'
+        ws.send(sendData)
+        time.sleep(10)
+    thread.start_new_thread(run, ())
+
 # All Flask app must create an app instance like this with the name of
 # the main module:
 
-DATABASE = 'deomps5.db'
 
-
-app = Flask(__name__)
-app.config.from_object(__name__)
 # DB connection
 def get_db():
     db = getattr(g, '_database', None)
@@ -49,17 +82,38 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+def realtime_connect():
+    ws = websocket.WebSocketApp("wss://api.upbit.com/websocket/v1",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    thread.start_new_thread(ws.run_forever, ())
 # Invoke this one with http://127.0.0.1:5000
 @app.route('/')
 def index():
-    headers = {"Authorization": authorization_token}
-    res = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-ETH', headers=headers).json()
-    print(res)
+    realtime_connect()
+#    headers = {"Authorization": authorization_token}
+#    res = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-ETH', headers=headers).json()
+#    print(res)
     return render_template('base.html')
 
+# Flask SocketIO handler
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
 
+@socketio.on('json')
+def handle_json(json):
+    emit(json, json=True)
+
+@socketio.on('message')
+def handle_message(message):
+    emit('json',message)
 
 # Now, run the app as a server in debug mode or public mode
 if __name__ == '__main__':
-    app.run(debug=True)        # Debug mode will reload files when changed.
+    wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
+    socketio.run(app)
+  #  app.run(debug=True)        # Debug mode will reload files when changed.
     # app.run(host='0.0.0.0')  # To make the server listen on all public IPs.
